702962b9efaaec0fe10c072717fddedeeaacc6bb
Justin Muncaster <jmuncaster@toyon.com>
2014-01-07 00:34:11 -0800

    Render word to spell and add clapping for completing a word

--- babygame.py
+++ babygame.py
@@ -5,6 +5,7 @@ from pygame.locals import *
 import pygame.mixer
 from time import time
 
+
 def initialize_screen(fullscreen=False):
   print "Initializing pygame"
   pygame.init()
@@ -31,6 +32,7 @@ def initialize_screen(fullscreen=False):
   screen = pygame.display.get_surface()
   return screen
 
+
 def get_input(events):
   "Get the keypress from the user"
   for event in events:
@@ -50,6 +52,16 @@ def get_input(events):
         return keypress
   return None
 
+
+def play_file(filename):
+  if os.path.exists(filename):
+    try:
+      pygame.mixer.music.load(filename)
+      pygame.mixer.music.play(0, 0.0)
+    except:
+      print "Error playing %s" % filename
+
+
 def play_voice_file(keypress):
   "Play the voice file for the provided keypress"
   dirname = keypress
@@ -57,8 +69,16 @@ def play_voice_file(keypress):
   voice_file = os.path.join("media", dirname, filename)
   print "voice_file = %s" % voice_file
   if os.path.exists(voice_file):
-    pygame.mixer.music.load(voice_file)
-    pygame.mixer.music.play(0, 0.0)
+    try:
+      pygame.mixer.music.load(voice_file)
+      pygame.mixer.music.play(0, 0.0)
+    except:
+      print "Error playing %s" % voice_file
+
+
+def play_clap_file():
+  play_file("media/clap.wav")
+
 
 def get_image(keypress):
   "Get image for the keypress, or None if not available"
@@ -81,14 +101,26 @@ def get_image(keypress):
 
 def random_color():
   "Get a random color"
-  r = random.randrange(128,256)
-  g = random.randrange(128,256)
-  b = random.randrange(128,256)
+  r = random.randrange(128,192)
+  g = random.randrange(128,192)
+  b = random.randrange(128,192)
   return (r,g,b)
 
 
+def random_word():
+  words = ["LUCY", "BALL", "APPLE", "OUTSIDE", "UP"]
+  return words[random.randrange(0, len(words))]
 
-def main(fullscreen=False):
+
+# Main
+def main(argv=[]):
+  "Main entry point"
+
+  # Parse command line args
+  fullscreen = False
+  for arg in sys.argv:
+    if arg == "-f" or arg == "--fullscreen":
+      fullscreen = True
 
   # Initialization
   screen = initialize_screen(fullscreen)
@@ -97,58 +129,103 @@ def main(fullscreen=False):
   last_keypress = None
   last_update_time = time()
   image = None
-  update = True
+  refresh = True
   update_period = .75
+  last_word = random_word()
+  remaining_word = last_word
+  letters_found = 0
+  new_word_queued = False
+  word_completed_time = time()
 
   # Main program loop
   while True:
     keypress = get_input(pygame.event.get())
 
-    if keypress and not os.path.exists(os.path.join("media",keypress)):
-      keypress = None
-
-    if keypress and last_keypress != keypress:
+    if keypress:
       play_voice_file(keypress)
       image = get_image(keypress)
-      update = True
+      refresh = True
       last_keypress = keypress
+      if remaining_word and keypress.upper() == remaining_word[0].upper():
+        letters_found += 1
+        remaining_word = last_word[letters_found:]
 
-    update = update or time() - last_update_time > update_period
-
-    if update:
-      update = False
+    if refresh or time() - last_update_time > update_period:
+      refresh = False
       last_update_time = time()
 
-      screen.fill(random_color())
+      if not remaining_word and not new_word_queued:
+        print "Completed word!!!"
+        word_completed_time = time()
+        new_word_queued = True
+        play_clap_file()
+
+      # Switch fill color and move sprites
+      background_color = random_color()
 
       if image and screen.get_rect().contains(image.get_rect()):
         x = random.randrange(0, screen.get_width() - image.get_width())
         y = random.randrange(0, screen.get_height() - image.get_height())
-        screen.blit(image, (x,y))
+
+    if new_word_queued and time() - word_completed_time > 1:
+      print "Getting new word"
+      last_word = random_word()
+      letters_found = 0
+      remaining_word = last_word
+      new_word_queued = False
 
     # Render
-    if last_keypress:
-      font = pygame.font.SysFont("Arial Black", 265)
-      
-      antialias = True
-      text_color = (0, 0, 0)
-      text = font.render(last_keypress.upper(), antialias, text_color)
+    screen.fill(background_color)
 
-      textpos = text.get_rect(
+    # Render image
+    if image:
+      screen.blit(image, (x,y))
+
+    # Text options
+    antialias = True
+
+    if letters_found > 0:
+      completed_word_color = (0, 192, 0)
+      completed_word_font = pygame.font.SysFont("Arial Black", 60)
+      completed_word_text = completed_word_font.render(last_word[:letters_found], antialias, completed_word_color)
+      completed_word_pos  = completed_word_text.get_rect(x=10, y=0)
+      screen.blit(completed_word_text, completed_word_pos)
+      cursor_x = completed_word_pos.x + completed_word_text.get_width()
+      cursor_y = completed_word_pos.y
+    else:
+      cursor_x = 10
+      cursor_y = 0
+
+    if letters_found < len(last_word):
+      current_letter_color = (255, 255, 255)
+      current_letter_font = pygame.font.SysFont("Arial Black", 60)
+      current_letter_text = current_letter_font.render(
+        last_word[letters_found],
+        antialias,
+        current_letter_color)
+      current_letter_pos  = current_letter_text.get_rect(x=cursor_x, y=cursor_y)
+      screen.blit(current_letter_text, current_letter_pos)
+      cursor_x = current_letter_pos.x + current_letter_text.get_width()
+      cursor_y = current_letter_pos.y      
+
+    word_color = (32, 32, 32)
+    word_font = pygame.font.SysFont("Arial Black", 60)
+    word_text = word_font.render(last_word[letters_found+1:], antialias, word_color)
+    word_pos  = word_text.get_rect(x=cursor_x, y=cursor_y)
+    screen.blit(word_text, word_pos)
+
+    if last_keypress:
+      letter_color = (0, 0, 0)
+      letter_font = pygame.font.SysFont("Arial Black", 265)      
+      letter_text = letter_font.render(last_keypress.upper(), antialias, letter_color)
+      letter_pos  = letter_text.get_rect(
         centerx=screen.get_width() / 2,
         centery=screen.get_height() / 2)
-
-      screen.blit(text, textpos)
+      screen.blit(letter_text, letter_pos)
 
     pygame.display.flip()
 
 
+# Main entry point
 if __name__ == "__main__":
-  
-  fullscreen = False
-  
-  for arg in sys.argv:
-    if arg == "-f" or arg == "--fullscreen":
-      fullscreen = True
-
-  main(fullscreen)
+  main(sys.argv)
